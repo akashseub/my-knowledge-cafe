c0c77fed463e3d71ea2f7d6b225c2a35e66d19ed
akashseub <miahmdakash55@gmail.com>
2023-10-09 12:08:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl font-bold text-center'>Hello React</h1>
+      
+    </>
+  )
+}
+
+export default App
